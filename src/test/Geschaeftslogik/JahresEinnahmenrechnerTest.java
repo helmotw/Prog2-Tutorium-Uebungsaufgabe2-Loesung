77cf5b7897000e5335905abfa9164a9741f90267
s0582080 <102999393+helmotw@users.noreply.github.com>
2022-11-06 20:04:23 +0100

    Update JahresEinnahmenrechnerTest.java

--- src/test/Geschaeftslogik/JahresEinnahmenrechnerTest.java
+++ src/test/Geschaeftslogik/JahresEinnahmenrechnerTest.java
@@ -12,6 +12,6 @@ class JahresEinnahmenrechnerTest {
     void berechnung() {
         KundenDB testdb=new KundenDB();
         testdb.hinzufuegen(new Kunde(37,"female",37.74,3,true,"northwest",1.0));
-        assertEquals(2.589203410794825,JahresEinnahmenrechner.berechnung(testdb));
+        assertEquals(0.5974989722274768,JahresEinnahmenrechner.berechnung(testdb));
     }
 }
